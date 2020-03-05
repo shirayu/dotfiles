e7f7a24cc297599ff209ecea490779acaf250110
Yuta Hayashibe <yuta@hayashibe.jp>
2020-03-05 21:03:42 +0900

    Updated

--- generate_links.sh
+++ generate_links.sh
@@ -1,39 +1,27 @@
 #!/usr/bin/env bash
 
+set -x
+
 SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ ! -e "${SRC}/.tmux" ];then
     echo "Not found: [${SRC}/.tmux]"
     exit 1
 fi
 
-ln -snf $SRC/.gitconfig ~/.gitconfig
-ln -snf $SRC/.screenrc ~/.screenrc
-ln -snf $SRC/.zsh ~/.zsh
-ln -snf $SRC/.zprofile ~/.zprofile
-ln -snf $SRC/.zlogout ~/.zlogout
-ln -snf $SRC/.zshrc ~/.zshrc
-
-ln -snf $SRC/fonts.conf ~/.fonts.conf
 mkdir -p ~/.config/fontconfig
-ln -snf $SRC/fonts.conf ~/.config/fontconfig/fonts.conf
+find "${SRC}" -mindepth 1 -maxdepth 1  -print0 -type f -name '\.*' | xargs -0 -I {} ln -snf {} ~
 
-ln -snf $SRC/.tmux.conf ~/.tmux.conf
-ln -snf $SRC/.tmux ~/.tmux
-ln -snf $SRC/.latexmkrc ~/.latexmkrc
-ln -snf $SRC/.clang-format ~/.clang-format
-ln -snf $SRC/.npmrc ~/.npmrc
-ln -snf $SRC/.eslintrc.json ~/.eslintrc.json
-ln -snf $SRC/.markdownlint.json ~/.markdownlint.json
-
-if [ -d $SRC/dot_vim ];then
-    ln -snf $SRC/dot_vim/ ~/.vim
+if [ -d "$SRC/dot_vim" ];then
+    ln -snf "$SRC/dot_vim/" ~/.vim
 fi
 if [ -f ~/.vim/vimrc ];then
     ln -snf ~/.vim/vimrc ~/.vimrc
 fi
 
 mkdir -p ~/local/bin
-ln -snf $SRC/bin/* ~/local/bin
+ln -snf "$SRC"/bin/* ~/local/bin
 
-wget --no-glob https://raw.githubusercontent.com/google/adb-sync/master/adb-sync -P ~/local/bin
+if [ ! -f ~/local/bin/adb-sync ];then
+    wget --no-glob https://raw.githubusercontent.com/google/adb-sync/master/adb-sync -P ~/local/bin
+fi
 chmod a+x ~/local/bin/adb-sync
