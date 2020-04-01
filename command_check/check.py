9b9ce93bd72a980fbf17f6fdc198a8710e165ad3
Yuta Hayashibe <yuta@hayashibe.jp>
2020-04-01 13:20:28 +0900

    Added

---
+++ command_check/check.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import argparse
+import codecs
+import os
+import platform
+import shutil
+import subprocess
+import sys
+import typing
+
+
+def brew_checks(targets: typing.List[str]) -> typing.Iterable[str]:
+    res = subprocess.run(["brew", "list"], stdout=subprocess.PIPE)
+    installed: typing.Set[str] = set()
+    for item in res.stdout.decode('utf8').split():
+        installed.add(item.split('@')[0])
+
+    for t in targets:
+        if t not in installed:
+            yield t
+
+
+def checks(targets: typing.List[str]) -> typing.Iterable[str]:
+    for t in targets:
+        if not shutil.which(t):
+            yield t
+
+
+def get_list(path_in: str) -> typing.List[str]:
+    with codecs.open(path_in, "r", "utf8") as inf:
+        targets = [line.strip()for line in inf]
+    return targets
+
+
+def operation(path_indir: str) -> bool:
+    def check() -> typing.Iterator[str]:
+        pf = platform.system()
+        if pf == 'Darwin':
+            if shutil.which('brew'):
+                for t in brew_checks(
+                        get_list(os.path.join(path_indir, 'brew.txt'))):
+                    yield t
+            else:
+                yield 'brew'
+        elif pf == 'Linux':
+            for t in checks(get_list(os.path.join(path_indir, 'linux.txt'))):
+                yield t
+        elif pf == 'Windows':
+            for t in checks(get_list(os.path.join(path_indir, 'windows.txt'))):
+                yield t
+        for t in checks(get_list(os.path.join(path_indir, 'all.txt'))):
+            yield t
+        # TODO npm pip
+
+    found = False
+    for t in check():
+        print(t)
+        found = True
+    return found
+
+
+def get_opts() -> argparse.Namespace:
+    script_dir: str = os.path.dirname(os.path.abspath(__file__))
+    oparser = argparse.ArgumentParser()
+    oparser.add_argument("--input", "-i", default=script_dir)
+    return oparser.parse_args()
+
+
+def main() -> None:
+    opts = get_opts()
+    found = operation(opts.input)
+    if found:
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
