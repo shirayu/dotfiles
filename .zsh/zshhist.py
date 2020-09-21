b7fcf45662bdbfc6eb1b8f632d6a9fa97c36838d
Yuta Hayashibe <yuta@hayashibe.jp>
2020-09-22 01:31:42 +0900

    Fix

--- .zsh/zshhist.py
+++ .zsh/zshhist.py
@@ -1,10 +1,11 @@
 import sys
+
 m = False
 for d in sys.stdin.read():
     c = ord(d)
     if c != 0x83:
-        d = c ^ 32 if m else c
-        sys.stdout.write(chr(d))
+        t = c ^ 32 if m else c
+        sys.stdout.write(chr(t))
         m = False
     else:
         m = True
