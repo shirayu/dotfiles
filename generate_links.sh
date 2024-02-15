48e80f1de20ea49c0c81ef90363fe088f594be12
Yuta Hayashibe <yuta@hayashibe.jp>
2024-02-15 15:52:08 +0900

    Add

--- generate_links.sh
+++ generate_links.sh
@@ -27,6 +27,6 @@ fi
 chmod a+x ~/local/bin/adb-sync
 
 # https://qiita.com/q1701/items/00418d17ec97cc2768f7
-mkdir -p ~/.config/fontconfig && ln -s "${SRC}/dot_config/fontconfig/fonts.conf" ~/.config/fontconfig
+mkdir -p ~/.config/fontconfig && ln -s "${SRC}/dot_config/fontconfig/fonts.conf" ~/.config/fontconfig/fonts.conf
 
-mkdir -p ~/.config/mise && ln -s "${SRC}/dot_config/mise/mise_config.toml" ~/.config/mise
+mkdir -p ~/.config/mise && ln -s "${SRC}/dot_config/mise/mise_config.toml" ~/.config/mise/config.toml
