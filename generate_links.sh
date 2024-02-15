937d45e0bb6e40591784accadfe7e22c79a60581
Yuta Hayashibe <yuta@hayashibe.jp>
2024-02-15 15:38:24 +0900

    Add font config

--- generate_links.sh
+++ generate_links.sh
@@ -25,3 +25,7 @@ if [ ! -f ~/local/bin/adb-sync ]; then
     wget --no-glob https://raw.githubusercontent.com/google/adb-sync/master/adb-sync -P ~/local/bin
 fi
 chmod a+x ~/local/bin/adb-sync
+
+# https://qiita.com/q1701/items/00418d17ec97cc2768f7
+mkdir -p ~/.config/fontconfig
+ln -s "${SRC}/dot_config/fontconfig/fonts.conf" ~/.config/fontconfig
