6674a0842e132e4bac57b6a366ff9e749f847557
Yuta Hayashibe <yuta@hayashibe.jp>
2024-03-25 13:39:13 +0900

    Format

--- .zsh/mysshagent.sh
+++ .zsh/mysshagent.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 SSH_AGENT_FILE=/tmp/.ssh-agent-$(hostname)-${USERNAME}
-/usr/bin/gnome-keyring-daemon --start --components=ssh > "${SSH_AGENT_FILE}"
-
+/usr/bin/gnome-keyring-daemon --start --components=ssh >"${SSH_AGENT_FILE}"
